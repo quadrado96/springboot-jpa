97dec52d256c3ed976137f90e316ee06badaae29
Brunno Quadrado <bbertoninuti@gmail.com>
2024-09-13 12:09:55 -0300

    Product

--- src/main/java/com/educandoweb/course1/entities/User.java
+++ src/main/java/com/educandoweb/course1/entities/User.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
