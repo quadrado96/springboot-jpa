7df790c408502eee2b7f7232c2ee2d064b4d2ca8
Brunno Quadrado <bbertoninuti@gmail.com>
2024-10-05 16:34:43 -0300

    Subtotal & Total methods

--- src/main/java/com/educandoweb/course1/entities/Order.java
+++ src/main/java/com/educandoweb/course1/entities/Order.java
@@ -102,6 +102,14 @@ public class Order implements Serializable{
 		return items;
 	}
 	
+	public Double getTotal() {
+		double sum = 0;
+		for (OrderItem orderItem: items) {
+			sum += orderItem.getSubTotal();
+		}
+		return sum;
+	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
