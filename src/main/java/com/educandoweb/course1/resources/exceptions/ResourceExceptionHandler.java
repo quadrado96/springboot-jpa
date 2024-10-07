b6db9063c0aedcf20f9406b0f53f12d667cbd9df
Brunno Quadrado <bbertoninuti@gmail.com>
2024-10-07 12:22:31 -0300

    Exception handling - delete

--- src/main/java/com/educandoweb/course1/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/educandoweb/course1/resources/exceptions/ResourceExceptionHandler.java
@@ -2,11 +2,13 @@ package com.educandoweb.course1.resources.exceptions;
 
 import java.time.Instant;
 
+import org.hibernate.boot.model.relational.Database;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.educandoweb.course1.services.exceptions.DatabaseException;
 import com.educandoweb.course1.services.exceptions.ResourceNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -18,6 +20,14 @@ public class ResourceExceptionHandler {
 	public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
 		String error = "Resource not found.";
 		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI()); 
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> databaseException(DatabaseException e, HttpServletRequest request) {
+		String error = "Database error.";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
 		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI()); 
 		return ResponseEntity.status(status).body(err);
 	}
